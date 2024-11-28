f21be31eb639bf61ae18bebadedbd015e62870aa
gustavoherdt10 <gusherdt@gmail.com.br>
2024-11-28 19:22:33 -0300

    e

--- trabalho/login.php
+++ trabalho/login.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once 'includes/config.php';
-require_once 'includes/funcoes.php';
+require_once '../includes/config.php';
+require_once '../includes/funcoes.php';
+
 
 session_start();
 
